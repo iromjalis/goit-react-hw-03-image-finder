b36eab627a8d43a8acb42e42e05652d588ad4f4b
Irina Romjalis <iromjalis@gmail.com>
2021-09-12 23:46:50 +0200

    search query

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
 //import { Test } from './Searchbar.styles';
 
-const Searchbar = (props) => (
-  <div className="SearchbarWrapper">
-    Test content
-  </div>
-);
+class Searchbar extends Component {
+  state = {
+    query: "",
+  };
+  handleChange = (e) => {
+    this.setState({ query: e.currentTarget.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+
+    this.setState({ query: "" });
+  };
+  render() {
+    const { onSubmit, onChange, value } = this.props;
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            value={this.state.query}
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
 
 Searchbar.propTypes = {
   // bla: PropTypes.string,
