c619e4d180bf6ed2b305d3815ee3413784e0dea9
Irina Romjalis <iromjalis@gmail.com>
2021-09-12 23:46:32 +0200

    modal basics

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,18 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+// import PropTypes from "prop-types";
 //import { Test } from './Modal.styles';
 
-class Modal extends PureComponent { 
-  constructor(props) {
-    super(props);
+class Modal extends PureComponent {
+  state = {
+    hasError: false,
+  };
 
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  componentWillMount = () => {
-    console.log('Modal will mount');
-  }
-
-  componentDidMount = () => {
-    console.log('Modal mounted');
-  }
-
-  componentWillReceiveProps = (nextProps) => {
-    console.log('Modal will receive props', nextProps);
-  }
-
-  componentWillUpdate = (nextProps, nextState) => {
-    console.log('Modal will update', nextProps, nextState);
-  }
-
-  componentDidUpdate = () => {
-    console.log('Modal did update');
-  }
-
-  componentWillUnmount = () => {
-    console.log('Modal will unmount');
-  }
-
-  render () {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
+  render() {
     return (
-      <div className="ModalWrapper">
-        Test content
+      <div className="Overlay">
+        <div className="Modal">
+          <img src="" alt="" />
+        </div>
       </div>
     );
   }
